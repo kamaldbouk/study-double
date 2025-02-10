ddc613d9897ebcaaba7143f03b7fac944604b505
kamaldbouk <kamaldbouk13@gmail.com>
2025-02-10 17:21:20 +0200

    initial commit

---
+++ frontend/src/Dashboard/SideBar/CreateRoomButton.js
@@ -0,0 +1,34 @@
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Groups';
+// import * as roomHandler from '../../realtimeCommunication/roomHandler;'
+import * as roomHandler from '../../realtimeCommunication/roomHandler';
+
+const CreateRoomButton = ({isUserInRoom}) => {
+
+    const createNewRoomHandler = () => {
+        //create a room and sending info to the server ab it
+        roomHandler.createNewRoom();
+    }
+    return ( 
+        <Button
+        disabled={isUserInRoom}
+        onClick={createNewRoomHandler}
+            style={{
+                width: "48px",
+                height: "48px",
+                borderRadius: "16px",
+                margin: 0,
+                padding: 0,
+                minWidth: 0,
+                marginTop: "10px",
+                color: "white",
+                backgroundColor: "#5865F2",
+                fontSize: '25px'
+            }}
+            >
+                +
+            </Button>
+     );
+}
+ 
+export default CreateRoomButton;
---
+++ frontend/src/realtimeCommunication/webRTCHandler.js
@@ -0,0 +1,140 @@
+import store from '../store/store';
+import { setLocalStream, setRemoteStreams } from '../store/actions/roomActions';
+import Peer from 'simple-peer';
+import * as socketConnection from './socketConnection';
+
+const getConfiguration = () => {
+    const turnIceServers = null;
+    if (turnIceServers) {
+        // TODO
+    } else {
+        console.log('Using only STUN server.');
+        return {
+            iceServers: [
+                {
+                    urls: 'stun:stun.l.google.com:19302'
+                },
+            ],
+        };
+    };
+};
+
+const onlyAudioConstraints = {
+    audio: true,
+    video: false,
+}
+
+const defaultConstraints = {
+    video: true,
+    audio: true,
+}
+
+export const getLocalStreamPeview = (onlyAudio = false, callbackFunc) => {
+    const constraints = onlyAudio ? onlyAudioConstraints : defaultConstraints;
+
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+        store.dispatch(setLocalStream(stream));
+        callbackFunc();
+    }).catch(err => {
+        console.log(err);
+        console.log('Cannot get an access to local stream.');
+    });
+};
+
+let peers = {};
+
+export const prepareNewPeerConnection = (connUserSocketId, isInitiator) => {
+    const localStream = store.getState().room.localStream;
+
+    if (isInitiator) {
+        console.log('Prep new peer connection as initiator');
+    }
+    else {
+        console.log('prep new peer con as not initiator');
+    }
+
+    peers[connUserSocketId] = new Peer({
+        initiator: isInitiator,
+        config: getConfiguration(),
+        stream: localStream,
+    });
+
+    peers[connUserSocketId].on('signal', data => {
+        const signalData = {
+            signal: data,
+            connUserSocketId: connUserSocketId,
+        };
+
+        socketConnection.signalPeerData(signalData);
+    });
+
+    peers[connUserSocketId].on('stream', (remoteStream) => {
+        console.log('remote stream came from other user');
+        console.log('direct conn has been establshed');
+        remoteStream.connUserSocketId = connUserSocketId;
+        addNewRemoteStream(remoteStream);
+    });
+};
+
+export const handleSignalingData = (data) => {
+    const { connUserSocketId, signal } = data;
+
+    if (peers[connUserSocketId]) {
+        peers[connUserSocketId].signal(signal);
+    }
+};
+
+const addNewRemoteStream = (remoteStream) => {
+    const remoteStreams = store.getState().room.remoteStreams;
+    const newRemoteStreams = [...remoteStreams, remoteStream];
+
+    store.dispatch(setRemoteStreams(newRemoteStreams));
+}
+
+export const closeAllConnections = () => {
+    Object.entries(peers).forEach((mappedObject) => {
+        const connUserSocketId = mappedObject[0];
+        if (peers[connUserSocketId]) {
+        peers[connUserSocketId].destroy();
+        delete peers[connUserSocketId];
+        }
+    });
+};
+
+
+export const handleParticipantLeftRoom = (data) => {
+    const { connUserSocketId } = data;
+  
+    if (peers[connUserSocketId]) {
+      peers[connUserSocketId].destroy();
+      delete peers[connUserSocketId];
+    }
+  
+    const remoteStreams = store.getState().room.remoteStreams;
+  
+    const newRemoteStreams = remoteStreams.filter(
+      (remoteStream) => remoteStream.connUserSocketId !== connUserSocketId
+    );
+  
+    store.dispatch(setRemoteStreams(newRemoteStreams));
+};
+
+export const switchOutgoingTracks = (stream) => {
+    for (let socket_id in peers) {
+        for (let index in peers[socket_id].streams[0].getTracks()) {
+            for (let index2 in stream.getTracks()) {
+            if (
+                peers[socket_id].streams[0].getTracks()[index].kind ===
+                stream.getTracks()[index2].kind
+            ) {
+                peers[socket_id].replaceTrack(
+                peers[socket_id].streams[0].getTracks()[index],
+                stream.getTracks()[index2],
+                peers[socket_id].streams[0]
+                );
+                break;
+            }
+            }
+        }
+    }
+};
